82cd1d3525428033211b186c78eb8a7955bc2e2a
Battle-with-bugs <daonguyen2941@gmail.com>
2024-04-21 22:23:34 +0700

    updete profile user

--- src/users/entity/UserProfile.entity.ts
+++ src/users/entity/UserProfile.entity.ts
@@ -13,6 +13,9 @@ export class Profile extends BaseEntity {
     @Column({ type: "varchar", nullable: false, unique: true })
     email: string;
 
+    @Column({ type: "varchar", nullable: true, default: 'https://vivu-world-cloud.s3.ap-southeast-1.amazonaws.com/image/anh-avatar-trang-tron.jpg' })
+    avatar: string;
+
     @Column({ type: "int", nullable: true, unique: true })
     phone: number;
 
@@ -21,4 +24,10 @@ export class Profile extends BaseEntity {
 
     @Column({ type: "date", nullable: true })
     birthday: Date;
+
+    @Column({ default: false })
+    isActive: boolean;
+
+    @Column({ type: "datetime", nullable: true })
+    LastLogin: Date;
 }
